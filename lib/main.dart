f6139ef2f954c1aaa8247023f865ff1fda7ffb9f
ibrahimsebbane <ibrahim.sebbane@gmail.com>
2024-03-26 12:19:19 +0100

    wifi and bluetooth done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:esc_pos_printer/esc_pos_printer.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:testing_printer/widgets/alert_dialog_printers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -52,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
     super.didChangeDependencies();
   }
 
-  void testReceipt(NetworkPrinter printer) {
+  void testReceiptWifi(NetworkPrinter printer) {
     printer.text('Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
     printer.text('Special 1: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ', styles: const PosStyles(codeTable: 'CP1252'));
     printer.text('Special 2: blåbærgrød', styles: const PosStyles(codeTable: 'CP1252'));
@@ -74,11 +74,41 @@ class _MyHomePageState extends State<MyHomePage> {
     printer.cut();
   }
 
+  // Ticket testReceiptBluetooth() {
+  //   // Ticket is not found we can use traditional bytes printing method
+  //   final Ticket ticket = Ticket(PaperSize.mm80);
+
+  //   ticket.text('Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
+  //   ticket.text('Special 1: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ', styles: PosStyles(codeTable: PosCodeTable.westEur));
+  //   ticket.text('Special 2: blåbærgrød', styles: PosStyles(codeTable: PosCodeTable.westEur));
+
+  //   ticket.text('Bold text', styles: PosStyles(bold: true));
+  //   ticket.text('Reverse text', styles: PosStyles(reverse: true));
+  //   ticket.text('Underlined text', styles: PosStyles(underline: true), linesAfter: 1);
+  //   ticket.text('Align left', styles: PosStyles(align: PosAlign.left));
+  //   ticket.text('Align center', styles: PosStyles(align: PosAlign.center));
+  //   ticket.text('Align right', styles: PosStyles(align: PosAlign.right), linesAfter: 1);
+
+  //   ticket.text('Text size 200%',
+  //       styles: PosStyles(
+  //         height: PosTextSize.size2,
+  //         width: PosTextSize.size2,
+  //       ));
+
+  //   ticket.feed(2);
+  //   ticket.cut();
+  //   return ticket;
+  // }
+
+  _showBluetoothPrinters() async {
+    await showDialog(context: context, builder: (_) => AlertDialogPrinters());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(190, 150, 85, 1),
+        backgroundColor: Color.fromARGB(190, 150, 85, 1),
         title: FittedBox(child: Text(widget.title, style: const TextStyle(color: Colors.white))),
       ),
       body: Row(
@@ -120,7 +150,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                       onChanged: (value) => port = value,
-                      validator: (value) => portRegEx!.hasMatch(value!) ? null : "Please type a valide IPV4 address",
+                      validator: (value) => portRegEx!.hasMatch(value!) ? null : "Please type a valide PORT number",
                       onSaved: (newValue) {
                         port = newValue;
                         print("prot:" + port!);
@@ -136,39 +166,64 @@ class _MyHomePageState extends State<MyHomePage> {
 
       floatingActionButton: _isLoading
           ? const CircularProgressIndicator()
-          : FloatingActionButton(
-              onPressed: () async {
-                final isValid = _formKey.currentState!.validate();
-                print("isValid:" + isValid.toString());
-                if (isValid) {
-                  print("port:" + port! + " ip:" + ipAddress!);
-                  _formKey.currentState!.save();
-                  setState(() {
-                    _isLoading = true;
-                  });
-                  const PaperSize paper = PaperSize.mm80;
-                  final profile = await CapabilityProfile.load();
-                  final printer = NetworkPrinter(paper, profile);
-                  // final PosPrintResult res = await printer.connect('192.168.0.123', port: 9100);
-                  final PosPrintResult res = await printer.connect(ipAddress!, port: int.parse(port!), timeout: Duration(seconds: 10));
-                  if (res == PosPrintResult.success) {
-                    testReceipt(printer);
-                    printer.disconnect();
-                    setState(() {
-                      response = "printing....";
-                      _isLoading = false;
-                    });
-                  } else {
-                    debugPrint('Print result: ${res.msg}');
-                    setState(() {
-                      response = res.msg;
-                      _isLoading = false;
-                    });
-                  }
-                }
-              },
-              tooltip: 'Print',
-              child: const Icon(Icons.print),
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  onPressed: () async {
+                    final isValid = _formKey.currentState!.validate();
+                    print("isValid:" + isValid.toString());
+                    if (isValid) {
+                      print("port:" + port! + " ip:" + ipAddress!);
+                      _formKey.currentState!.save();
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      const PaperSize paper = PaperSize.mm80;
+                      final profile = await CapabilityProfile.load();
+                      final printer = NetworkPrinter(paper, profile);
+                      // final PosPrintResult res = await printer.connect('192.168.0.123', port: 9100);
+                      final PosPrintResult res = await printer.connect(ipAddress!, port: int.parse(port!), timeout: Duration(seconds: 10));
+                      if (res == PosPrintResult.success) {
+                        testReceiptWifi(printer);
+                        printer.disconnect();
+                        setState(() {
+                          response = "printing....";
+                          _isLoading = false;
+                        });
+                      } else {
+                        debugPrint('Print result: ${res.msg}');
+                        setState(() {
+                          response = res.msg;
+                          _isLoading = false;
+                        });
+                      }
+                    }
+                  },
+                  tooltip: 'Print',
+                  child: const Column(
+                    children: [
+                      Icon(Icons.wifi, color: Colors.blue),
+                      Icon(Icons.print, color: Colors.blue),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 15),
+                FloatingActionButton(
+                  backgroundColor: Colors.blue[900],
+                  onPressed: () async {
+                    _showBluetoothPrinters();
+                  },
+                  tooltip: 'Print',
+                  child: const Column(
+                    children: [
+                      Icon(Icons.bluetooth, color: Colors.white),
+                      Icon(Icons.print, color: Colors.white),
+                    ],
+                  ),
+                ),
+              ],
             ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
